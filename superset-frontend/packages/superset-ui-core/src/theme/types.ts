bfd32e007a8f02ae4a0064b8edf40ad341bac640
VasilijeBursac <vasilijebursac@gmail.com>
2025-08-08 22:29:55 +0200

    feat(login): add dynamic background and branding support

--- superset-frontend/packages/superset-ui-core/src/theme/types.ts
+++ superset-frontend/packages/superset-ui-core/src/theme/types.ts
@@ -153,6 +153,8 @@ export interface SupersetSpecificTokens {
   brandLogoMargin: string;
   brandLogoHref: string;
   brandLogoHeight: string;
+  loginPageBackgroundImageUrl?: string;
+  loginFormBrandLogoUrl?: string;
 }
 
 /**
